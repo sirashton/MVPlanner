ba6e0a010203cdda67225577e15b1c3b27e8b033
Alex <a.ashton@hotmail.co.uk>
2024-10-12 19:27:59 +0100

    Basic Webview of JSON file

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,46 @@
-console.log('Extension file is being loaded');
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Activating extension "mvplanner"...');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Activating extension "mvplanner"...');
+    let disposable = vscode.commands.registerCommand('mvplanner.openPlanViewer', () => {
+        console.log('openPlanViewer command triggered');
+        const panel = vscode.window.createWebviewPanel(
+            'planViewer',
+            'Plan Viewer',
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true
+            }
+        );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('mvplanner.helloWorld', () => {
-		console.log('Hello World command was triggered!');
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MVPlanner!');
-	});
+        const planPath = path.join(context.extensionPath, 'plan.json');
+        const planData = fs.readFileSync(planPath, 'utf8');
 
-	context.subscriptions.push(disposable);
+        panel.webview.html = getWebviewContent(planData);
+    });
 
-	console.log('Registered mvplanner.helloWorld command');
-
-	// Try to execute the command programmatically
-	vscode.commands.executeCommand('mvplanner.helloWorld').then(
-		() => console.log('Successfully executed helloWorld command'),
-		(error) => console.error('Failed to execute helloWorld command:', error)
-	);
-
-	console.log('Congratulations, your extension "mvplanner" is now active!');
+    context.subscriptions.push(disposable);
+    console.log('mvplanner.openPlanViewer command registered');
 }
 
-// This method is called when your extension is deactivated
+function getWebviewContent(planData: string) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Plan Viewer</title>
+    </head>
+    <body>
+        <h1>Project Plan</h1>
+        <pre id="planData">${planData}</pre>
+    </body>
+    </html>`;
+}
+
 export function deactivate() {
-	console.log('Deactivating extension "mvplanner"...');
+    console.log('Deactivating extension "mvplanner"...');
 }
